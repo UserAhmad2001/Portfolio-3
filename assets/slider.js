08534323e245a63592feb1785eca5d012188e8a3
Ahmad <Kingahmed19991@gmail.com>
2022-10-29 11:09:13 +0300

    completed setting up the project slider

---
+++ assets/slider.js
@@ -0,0 +1,34 @@
+var pos = 0;
+
+var leftArrow = document.querySelector(".left-arrow")
+var rightArrow = document.querySelector(".right-arrow")
+var projects = document.querySelectorAll(".project")
+leftArrow.addEventListener('click',arrowClick);
+rightArrow.addEventListener('click',arrowClick);
+
+function arrowClick(ev){
+        if((ev.currentTarget===rightArrow) && pos!==-2){
+            pos--
+            projects.forEach(el=>{
+                el.style.transform = `translateX(${pos*100}%)`
+            })
+        }
+        else if((ev.currentTarget===leftArrow) && pos!==2){
+            pos++
+            projects.forEach(el=>{
+                el.style.transform = `translateX(${pos*100}%)`
+            })
+        }
+        else if(pos===2){
+            pos=0
+            projects.forEach(el=>{
+                el.style.transform = `translateX(${pos*100}%)`
+            })
+        }
+        else if(pos===-2){
+            pos=0
+            projects.forEach(el=>{
+                el.style.transform = `translateX(${pos*100}%)`
+            })
+        }
+}
